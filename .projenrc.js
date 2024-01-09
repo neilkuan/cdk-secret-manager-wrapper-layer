f5ae7b964d2c5353735ed5536d767c41d90349ff
Neil Kuan <guan840912@gmail.com>
2024-01-09 20:49:36 +0800

    chore: upgrade node version

--- .projenrc.js
+++ .projenrc.js
@@ -40,7 +40,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
     distName: 'cdk-secret-manager-wrapper-layer',
     module: 'cdk_secret_manager_wrapper_layer',
   },
-  workflowNodeVersion: '^16.20.0',
+  workflowNodeVersion: '^18',
   devDeps: [
     'typescript@4.6',
     'mock-fs@5.1.2',
